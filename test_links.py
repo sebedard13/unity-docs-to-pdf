cd6367416586eaa8e3f09208b1c6f36c92a41a22
Sébastien Bédard <sebedard13@gmail.com>
2023-03-23 11:07:38 -0400

    correct link test

---
+++ test_links.py
@@ -0,0 +1,62 @@
+import os
+
+import requests as requests
+from bs4 import BeautifulSoup
+
+
+def check_id(html: BeautifulSoup) -> tuple[list[str], list[str]]:
+    id_map = []
+    id_map_doublon = []
+    for elementId in html.select("*[id]"):
+        if elementId.attrs["id"] in id_map:
+            id_map_doublon.append(elementId.attrs["id"])
+        else:
+            id_map.append(elementId.attrs["id"])
+    return id_map, id_map_doublon
+
+
+def check_links(html, folder_path="", id_map=None) -> list[str]:
+    if id_map is None:
+        id_map = []
+
+    bad_href = []
+    for a_element in html.select("a[href]"):
+        href = a_element.attrs["href"]
+
+        if href.startswith("#"):
+            if not href.removeprefix("#") in id_map:
+                bad_href.append(href)
+
+        elif href.startswith("http"):
+            try:
+                r = requests.head(href)
+                if 400 <= r.status_code < 500:
+                    bad_href.append(href)
+            except requests.ConnectionError:
+                bad_href.append(href)
+            pass
+        else:
+            # I guess a file to found
+            filename = os.path.join(folder_path, href)
+            bad_href.append(filename)
+
+    return bad_href
+
+
+if __name__ == '__main__':
+
+    folder_path = "out/"
+    filename = folder_path + "out.html"
+
+    with open(filename, encoding="utf-8") as fp:
+        html = BeautifulSoup(fp, features="html.parser")
+
+    id_map, id_map_doublon = check_id(html)
+
+    for id_doublon in id_map_doublon:
+        print("Id doublon", id_doublon)
+
+    bad_href = check_links(html, id_map=id_map, folder_path=folder_path)
+
+    for href in bad_href:
+        print("Bad link", href)
